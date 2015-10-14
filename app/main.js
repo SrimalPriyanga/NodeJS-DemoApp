046bd01e6802f3dae8100d72a75b41dd93e673f3
Srimal Priyanga <s.priyanga22@gmail.com>
2015-10-14 11:35:33 +0530

    Node.js - Callbacks Concept

--- app/main.js
+++ app/main.js
@@ -1,3 +1,16 @@
 /**
  * Created by priyanga on 10/14/15.
  */
+var fs = require("fs");
+
+//Blocking Code Example
+//var data = fs.readFileSync('input.txt');
+//console.log(data.toString());
+
+//Non-Blocking Code Example
+fs.readFile('input.txt', function (err, data) {
+	if (err) return console.error(err);
+	console.log(data.toString());
+});
+
+console.log("Program Ended");
